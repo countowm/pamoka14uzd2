9ea9e4669229e4d1855042ea93e805d9dcaa8d59
countowm <countowm@gmail.com>
2022-06-08 08:42:36 +0300

    padariau

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    let a;
+    let b;
+    let h;
+    let V;
+
+    a = 9;
+    b = 84;
+    h = 94;
+
+    V = staciakampioGretasienoTuris(a*b*h);
+    console.log(V);
+}
+function staciakampioGretasienoTuris(V){
+    return V;
+}
